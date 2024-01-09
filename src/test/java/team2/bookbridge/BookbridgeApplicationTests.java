480dfa678fb8425aa65f0f406225a4fecc65a49d
kang <kang6@ewhain.net>
2024-01-09 16:21:14 +0900

    chore: 초기 프로젝트 생성

---
+++ src/test/java/team2/bookbridge/BookbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.bookbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
