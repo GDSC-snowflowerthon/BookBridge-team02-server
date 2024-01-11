7ee04cc20f5a4f0c46c0070cf6c20f2590eb4b08
kang <kang6@ewhain.net>
2024-01-12 02:33:47 +0900

    chore: 사용하지 않는 코드 주석

--- src/main/java/team2/bookbridge/global/config/WebMvcConfig.java
+++ src/main/java/team2/bookbridge/global/config/WebMvcConfig.java
@@ -3,15 +3,15 @@ package team2.bookbridge.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import team2.bookbridge.global.config.handler.UserInfoArgumentResolver;
+//import team2.bookbridge.global.config.handler.UserInfoArgumentResolver;
 
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new UserInfoArgumentResolver());
-    }
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(new UserInfoArgumentResolver());
+//    }
 }
